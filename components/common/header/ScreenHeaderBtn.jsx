aaaeef9ba98b9e7a96121cbe1a5260f8b2d708ae
tejas <tejasshinde815.com>
2023-04-27 11:47:35 +0530

    job details and search added

--- components/common/header/ScreenHeaderBtn.jsx
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -1,15 +1,14 @@
-import React from "react";
-import { TouchableOpacity, Image } from "react-native";
+import { Image, TouchableOpacity } from "react-native";
 
 import styles from "./screenheader.style";
 
-const ScreenHeaderBtn = ({ iconurl, dimention, handlePress }) => {
+const ScreenHeaderBtn = ({ iconUrl, dimension, handlePress }) => {
   return (
     <TouchableOpacity style={styles.btnContainer} onPress={handlePress}>
       <Image
-        source={iconurl}
+        source={iconUrl}
         resizeMode="cover"
-        style={styles.btnImg(dimention)}
+        style={styles.btnImg(dimension)}
       />
     </TouchableOpacity>
   );
